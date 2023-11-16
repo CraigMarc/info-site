67961db35b2c7d40aa6b2988a7b496e579b9b35b
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-11-16 17:50:13 -0500

    got rid of console

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
 http
   .createServer(function (req, res) {
     const q = url.parse(req.url, true);
-    console.log(q.pathname)
+    //console.log(q.pathname)
     let filename = "";
     if (q.pathname === "/") {
       filename = "." + "/index.html";
@@ -36,22 +36,3 @@ http
     });
   })
   .listen(8080);
-/*
-  const http = require('http');
-const url = require('url');
-const fs = require('fs');
-
-http.createServer(function (req, res) {
-  const q = url.parse(req.url, true);
-  const filename = q.pathname === "/" ? `./index.html` : `.${q.pathname}.html`
-
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end(`<h1>Error: 404</h1><p>The page you are looking for doesn't exist.</p> <a href="/">Back home.</a>`);
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);*/
